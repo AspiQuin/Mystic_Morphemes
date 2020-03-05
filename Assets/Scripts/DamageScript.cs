ade23e53f9763126c9cb7c801b4fec84fea13d7c
AspiQuin <32822827+AspiQuin@users.noreply.github.com>
2020-03-05 10:20:28 -0500

    Basic Health Implementation

---
+++ Assets/Scripts/DamageScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageScript : MonoBehaviour
+{
+    void takeDamage(int damage)
+    {
+        //this will contact the Player Health variable to reduce the number of hearts displayed
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthDisplay : MonoBehaviour
+{
+
+    int health = 3; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       for(int i =0; i < 3; i++)
+        {
+            if(i <= health)
+            {
+                GetComponentInChildren<i>()
+            }
+        }
+    }
+}
